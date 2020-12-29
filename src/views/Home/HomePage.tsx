aeaaa75d107c89ff1fe29ecad7ecfa301d9ab524
Tekena Solomon <tekena157@gmail.com>
2020-12-29 20:11:45 +0100

    Add some content to homepage

--- src/views/Home/HomePage.tsx
+++ src/views/Home/HomePage.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcome: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+});
 
 const HomePage = () => {
   return (
-    <View>
-      <Text>homepage</Text>
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome!</Text>
+      <Text>IOT automated animal feeder app</Text>
     </View>
   );
 };
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,16 +3,23 @@
 // export { Home } from "./Home";
 
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Route } from "react-router-native";
 import AppBar from "../components/AppBar";
 import { Drinkers } from "./Drinker";
 import { Feeders } from "./Feeder";
 import { Home } from "./Home";
 
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+  },
+});
+
 const Index = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <AppBar />
       <Route exact path="/" component={Home} />
       <Route path="/feeder" component={Feeders} />
